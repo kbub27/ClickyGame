7422e1c6cc9a21af84d765d53ce1f3943d130d96
kbub27 <kalebmerriman27@gmail.com>
2019-07-07 22:22:10 -0700

    initial handlers set up

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Column";
+import friends from "./friends.json";
 
-function App() {
-  
+function ranFriends(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+class App extends Component {
+    // SET INITIAL STATE
+    state = {
+        friends,
+        answer: '',
+        topScore: 0,
+        currentScore: 0,
+        clicked: []
+    };
+
+    //HANDLE TO SHUFFLE UP THE FRIENDS
+    handleShuffle = () => {
+        let shuffledFriends = ranFriends(friends);
+        this.setState({ friends: shuffledFriends });
+    };
+
+    //HANDLE THE CLICKS
+    handleClick = id => {
+
+    };
+
+    //HANDLE TH ESCORE INCREASE OR RESET AND TOPSCORE
+    handleScore = () => {
+
+    };
+
+    //HANDLE THE RESET OF THE GAME
+    handleReset = () => {
+
+    };
+
+
 }
 
 export default App;
