250e2ed2a6466e577ca8b67314f227b4182aa5a2
kbub27 <kalebmerriman27@gmail.com>
2019-07-07 23:30:43 -0700

    functionality added to friend card

--- clicky/src/components/friendcard/friendcard.js
+++ clicky/src/components/friendcard/friendcard.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+const FriendCard = props => {
+    return (
+        <div 
+        className="card"
+        value={props.id}
+        onClick={() => props.handleClick(props.id)} 
+        >
+            <img src={props.image} className="card-img-top" alt={props.name} />
+            <div className="card-body">
+                <h5 className="card-title text-center">{props.name}</h5>
+            </div>
+        </div>
+    )
+};
+
+export default FriendCard;
