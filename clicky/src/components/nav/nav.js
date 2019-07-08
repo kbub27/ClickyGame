8a7e3bf866a5ab16a01e9fa3ea419626ad999862
kbub27 <kalebmerriman27@gmail.com>
2019-07-07 23:16:40 -0700

    set up the navbar

--- clicky/src/components/nav/nav.js
+++ clicky/src/components/nav/nav.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+const Nav = props => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="/">{props.title}</a>
+            <ul className="navbar-nav justify-content-center">
+                <li className="nav-item">
+                    {props.answer}
+                </li>
+                <li className="nav-item">
+                    Top Score: {props.topScore} | Current Score: {props.currentScore}
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
--- clicky/src/components/title/title.js
+++ clicky/src/components/title/title.js
@@ -1,1 +1,5 @@
 import React from 'react';
+
+const Title = props => <h1 className='title'>{props.children}</h1>
+
+export default Title;
